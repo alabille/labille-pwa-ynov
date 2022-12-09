f7b7093d61b4da33960a9251faf56d591faa197a
Auriane Labille <auriane.labille@gmail.com>
2022-12-09 09:46:18 +0100

    Update database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb://localhost:27017/ynov-pwa")
+// mongoose.connect("mongodb://localhost:27017/ynov-pwa")
+mongoose.connect(`mongodb+srv://ynov-pwa:${process.env.DB_PASSWORD}@ynov-pwa.xknnnxv.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => console.log('✅ Successfully connected to the database'))
   .catch((e) => console.log(`⛔️ Error during database connection ${e}`))

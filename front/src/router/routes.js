b66d6bcb39883be2e95be894787bfd32abc6c790
Auriane Labille <auriane.labille@gmail.com>
2023-01-10 19:41:06 +0100

    Update routes.js

--- front/src/router/routes.js
+++ front/src/router/routes.js
@@ -14,23 +14,6 @@ const routes = [
     ]
   },
   {
-    path: '/quasar',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
-  },
-  {
-    path: '/tuto',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: 'v2', component: () => import('pages/TutoPage.vue') },
-      {
-        path: 'v3', component: () => import('pages/TutoPageVue3.vue')
-      }
-    ]
-  },
-  {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
   }
